ac15557700d2b95f736fce21920a9b301b8d7041
Steven Levine <slevine111@gmail.com>
2019-01-12 21:23:13 -0500

    Added beforeEach to test spec

--- showManagementStructure.spec.js
+++ showManagementStructure.spec.js
@@ -2,33 +2,34 @@ const expect = require('chai').expect
 const showManagementStructure = require('./showManagementStructure')
 
 describe('showManagementStructure function testing', () => {
-  it('showManagementStructure is a function', () => {
-    expect(typeof showManagementStructure).to.equal('function')
-  })
-  it('showManagementStructure returns a string', () => {
-    const users = [
+  let users
+  beforeEach(() => {
+    users = [
       { id: 1, name: 'moe' },
       { id: 2, name: 'larry', managerId: 1 },
       { id: 3, name: 'curly', managerId: 2 },
       { id: 4, name: 'shep', managerId: 1 },
       { id: 5, name: 'groucho', managerId: 4 }
     ]
+  })
+
+  it('showManagementStructure is a function', () => {
+    expect(typeof showManagementStructure).to.equal('function')
+  })
+  it('showManagementStructure returns a string', () => {
     expect(typeof showManagementStructure(users)).to.equal('string')
   })
   it('showManagementStructure returns the correct result', () => {
     const oneUser = [{ id: 1, name: 'moe' }]
     expect(showManagementStructure(oneUser)).to.equal('\nmoe')
-
-    const users = [
-      { id: 1, name: 'moe' },
-      { id: 2, name: 'larry', managerId: 1 },
-      { id: 3, name: 'curly', managerId: 2 },
-      { id: 4, name: 'shep', managerId: 1 },
-      { id: 5, name: 'groucho', managerId: 4 }
-    ]
     let result = showManagementStructure(users)
     expect(result).to.equal(
-      '\nmoe\n    - larry\n            - curly\n    - shep\n           - groucho'
+      `
+moe
+    - larry
+            - curly
+    - shep
+           - groucho`
     )
   })
 })
