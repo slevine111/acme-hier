03248dbe368d063ccfd11309dc1437a4816b854b
Steven Levine <slevine111@gmail.com>
2019-01-15 01:29:26 -0500

    Recoded function to work in all cases and added more testing - code definitely needs to be refactored

--- showManagementStructure.js
+++ showManagementStructure.js
@@ -1,21 +1,33 @@
 let showManagementStructure = input => {
-  let copyOfInput = [...input]
-  let string = ''
-  let lengthOfManagerLine
-  let stringForElement
-  copyOfInput.forEach((element, index, arr) => {
-    if (index === 0) {
-      element.numChars = element.name.length
-      stringForElement = element.name
-    } else {
-      lengthOfManagerLine = arr[element.managerId - 1].numChars
-      stringForElement = `${' '.repeat(lengthOfManagerLine + 1)}- ${
-        element.name
-      }`
-      element.numChars = stringForElement.length
-    }
-    string = `${string}\n${stringForElement}`
-  })
+  input = [...input]
+  let bossId =
+    input.findIndex(element => element.currentBossId) === -1
+      ? input.filter(element => !element.managerId)[0].id
+      : input[0].currentBossId
+  let currentLength =
+    input.findIndex(element => element.currentLength) === -1
+      ? 0
+      : input[0].currentLength
+  let string = `${currentLength ? `${' '.repeat(currentLength + 1)}- ` : '\n'}${
+    input.filter(element => element.id === bossId)[0].name
+  }`
+  currentLength = currentLength ? string.length : string.length - 1
+  let peopleBelow = input
+    .filter(user => user.managerId === bossId)
+    .map(user => user.id)
+  if (peopleBelow.length === 0) {
+    return string
+  }
+  for (let i = 0; i < peopleBelow.length; i++) {
+    string = `${string}\n${showManagementStructure(
+      input.map(element => {
+        return Object.assign({}, element, {
+          currentBossId: peopleBelow[i],
+          currentLength
+        })
+      })
+    )}`
+  }
   return string
 }
 
--- showManagementStructure.spec.js
+++ showManagementStructure.spec.js
@@ -2,26 +2,25 @@ const expect = require('chai').expect
 const showManagementStructure = require('./showManagementStructure')
 
 describe('showManagementStructure function testing', () => {
-  let users
-  beforeEach(() => {
-    users = [
+  it('showManagementStructure is a function', () => {
+    expect(typeof showManagementStructure).to.equal('function')
+  })
+  it('showManagementStructure returns a string', () => {
+    let users = [{ id: 1, name: 'moe' }]
+    expect(typeof showManagementStructure(users)).to.equal('string')
+  })
+  it('showManagementStructure returns the correct result with one user', () => {
+    let users = [{ id: 1, name: 'moe' }]
+    expect(showManagementStructure(users)).to.equal('\nmoe')
+  })
+  it('showManagementStructure retuns the correct when Id 1 is the ultimate boss', () => {
+    let users = [
       { id: 1, name: 'moe' },
       { id: 2, name: 'larry', managerId: 1 },
       { id: 3, name: 'curly', managerId: 2 },
       { id: 4, name: 'shep', managerId: 1 },
       { id: 5, name: 'groucho', managerId: 4 }
     ]
-  })
-
-  it('showManagementStructure is a function', () => {
-    expect(typeof showManagementStructure).to.equal('function')
-  })
-  it('showManagementStructure returns a string', () => {
-    expect(typeof showManagementStructure(users)).to.equal('string')
-  })
-  it('showManagementStructure returns the correct result', () => {
-    const oneUser = [{ id: 1, name: 'moe' }]
-    expect(showManagementStructure(oneUser)).to.equal('\nmoe')
     let result = showManagementStructure(users)
     expect(result).to.equal(
       `
@@ -30,6 +29,28 @@ moe
             - curly
     - shep
            - groucho`
+    )
+  })
+  it('showManagementStrcuture retuns the correct result even if Id 1 is not the ultimate boss', () => {
+    const users = [
+      { id: 1, name: 'moe', managerId: 6 },
+      { id: 2, name: 'larry', managerId: 5 },
+      { id: 3, name: 'curly', managerId: 1 },
+      { id: 4, name: 'shep', managerId: 1 },
+      { id: 5, name: 'groucho', managerId: 1 },
+      { id: 6, name: 'grouchot' },
+      { id: 20, name: 'ty', managerId: 3 }
+    ]
+    let result = showManagementStructure(users)
+    expect(result).to.equal(
+      `
+grouchot
+         - moe
+               - curly
+                       - ty
+               - shep
+               - groucho
+                         - larry`
     )
   })
 })
